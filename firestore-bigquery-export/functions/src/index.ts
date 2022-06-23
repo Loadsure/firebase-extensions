ab31e88b93710acc3ee4291f587aae692d430919
juan18valencia <juan.valencia@loadsure.net>
2022-06-23 08:45:51 -0500

    changed firebase tracker location

--- firestore-bigquery-export/functions/src/index.ts
+++ firestore-bigquery-export/functions/src/index.ts
@@ -20,7 +20,7 @@ import {
   ChangeType,
   FirestoreBigQueryEventHistoryTracker,
   FirestoreEventHistoryTracker,
-} from "./firestore-bigquery-change-tracker/src/index";
+} from "@firebaseextensions/firestore-bigquery-change-tracker";
 import * as logs from "./logs";
 import { getChangeType, getDocumentId } from "./util";
 
